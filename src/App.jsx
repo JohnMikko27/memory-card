7559f90f17f9a2c79aecbd599d6d1b5de98caede
Mikko <realmiks27@gmail.com>
2024-01-19 09:20:23 -0800

    Add linting rules and lint on save

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       hello world by mikkoss
     </>
-  )
+  );
 }
 
-export default App
+export default App;
