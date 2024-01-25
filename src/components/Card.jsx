640e5c663dba55afe2c140cc4f4cde48a034063b
Mikko <realmiks27@gmail.com>
2024-01-24 16:19:27 -0800

    Add pokemon names to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,10 @@
 import "../styles/Card.css";
 
-const Card = ({src, handleClickCard, id}) => {
+const Card = ({src, handleClickCard, id, name}) => {
   return (
     <div className="card">
       <img src={src} alt="" onClick={handleClickCard} id={id} />
+      <div className="name">{name.toUpperCase()}</div>
     </div>
   );
 };
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -7,7 +7,7 @@ const CardContainer = ({handleClickCard, images}) => {
     <div className="cardContainer">
       {
         images.map((image) => <Card src={image.sprites.front_default} key={image.id} 
-          id={image.id} handleClickCard={handleClickCard}/>)
+          id={image.id} handleClickCard={handleClickCard} name={image.name}/>)
       }
     </div>
   );
